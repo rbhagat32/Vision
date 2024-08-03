935f713844a267b7871d8456b250205ee2e32e33
rbhagat32 <raghavbhagat32@gmail.com>
2024-08-04 01:56:30 +0530

    implemented react lazy loading

--- src/utils/Routing.jsx
+++ src/utils/Routing.jsx
@@ -1,33 +1,36 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
-import Home from "../pages/Home";
-import Trending from "../pages/Trending";
-import Popular from "../pages/Popular";
-import Movies from "../pages/Movies";
-import MovieDetails from "../pages/MovieDetails";
-import TvShows from "../pages/TvShows";
-import TvShowDetails from "../pages/TvShowDetails";
-import People from "../pages/People";
-import PersonDetails from "../pages/PersonDetails";
-import Contact from "../pages/Contact";
-import NotFound from "../partials/NotFound";
+import Loading from "./Loading";
+const Home = lazy(() => import("../pages/Home"));
+const Trending = lazy(() => import("../pages/Trending"));
+const Popular = lazy(() => import("../pages/Popular"));
+const Movies = lazy(() => import("../pages/Movies"));
+const MovieDetails = lazy(() => import("../pages/MovieDetails"));
+const TvShows = lazy(() => import("../pages/TvShows"));
+const TvShowDetails = lazy(() => import("../pages/TvShowDetails"));
+const People = lazy(() => import("../pages/People"));
+const PersonDetails = lazy(() => import("../pages/PersonDetails"));
+const Contact = lazy(() => import("../pages/Contact"));
+const NotFound = lazy(() => import("../partials/NotFound"));
 
 export default function Routing() {
   const loc = useLocation();
 
   return (
-    <Routes location={loc} key={loc.pathname}>
-      <Route path="/" element={<Home />} />
-      <Route path="/trending" element={<Trending />} />
-      <Route path="/popular" element={<Popular />} />
-      <Route path="/movies" element={<Movies />} />
-      <Route path="/movie/:id" element={<MovieDetails />} />
-      <Route path="/tv-shows" element={<TvShows />} />
-      <Route path="/tv/:id" element={<TvShowDetails />} />
-      <Route path="/people" element={<People />} />
-      <Route path="/person/:id" element={<PersonDetails />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Suspense fallback={<Loading height="h-[100vh]" size="size-14" />}>
+      <Routes location={loc} key={loc.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/trending" element={<Trending />} />
+        <Route path="/popular" element={<Popular />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+        <Route path="/tv-shows" element={<TvShows />} />
+        <Route path="/tv/:id" element={<TvShowDetails />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/person/:id" element={<PersonDetails />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
   );
 }
