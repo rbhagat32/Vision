5565e6d9724873a54b8d94c1a661b0c2594b397b
rbhagat32 <raghavbhagat32@gmail.com>
2024-08-06 03:09:03 +0530

    added spacers on movie and tv details

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -254,6 +254,9 @@ export default function MovieDetails() {
           </div>
 
           {movie?.reviews?.length > 0 && <Reviews reviews={movie?.reviews} />}
+
+          {/* spacer */}
+          <div className="h-20"></div>
         </div>
       </div>
     </>
--- src/pages/TvShowDetails.jsx
+++ src/pages/TvShowDetails.jsx
@@ -254,6 +254,9 @@ export default function TvShowDetails() {
           </div>
 
           {tvShow?.reviews?.length > 0 && <Reviews reviews={tvShow?.reviews} />}
+
+          {/* spacer */}
+          <div className="h-20"></div>
         </div>
       </div>
     </>
