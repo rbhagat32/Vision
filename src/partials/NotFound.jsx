76f309f0a1b9da0ce4b6312ba1e52067f7e2d2ee
rbhagat32 <raghavbhagat32@gmail.com>
2024-08-08 22:25:46 +0530

    404 page added

--- src/partials/NotFound.jsx
+++ src/partials/NotFound.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <img src="/404.svg" />
+      <h1 className="text-4xl font-semibold text-rose-400 mb-3">
+        Page Not Found 😵
+      </h1>
+      <Link to="/" className="text-lg text-zinc-400">
+        Go back to Home
+      </Link>
+    </div>
+  );
 }
